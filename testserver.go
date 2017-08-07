51e04676ffe01ef480da59d19d24dc7885149cb0
Piotr Kowalczuk <p.kowalczuk@travelaudience.com>
2017-08-07 14:55:47 +0200

    test server change proposal, client is able to overload server if written wrong way

--- testserver.go
+++ testserver.go
@@ -1,36 +1,85 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"flag"
 	"log"
+	"math"
 	"math/rand"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 	"time"
 )
+
+var (
+	limit     int64
+	threshold int64 = 20
+)
+
+type GeneratorFunc func(context.Context, int) ([]int, error)
 
 func main() {
 	listenAddr := flag.String("http.addr", ":8090", "http listen address")
 	flag.Parse()
 
-	http.HandleFunc("/primes", handler([]int{2, 3, 5, 7, 11, 13}))
-	http.HandleFunc("/fibo", handler([]int{1, 1, 2, 3, 5, 8, 13, 21}))
-	http.HandleFunc("/odd", handler([]int{1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23}))
-	http.HandleFunc("/rand", handler([]int{5, 17, 3, 19, 76, 24, 1, 5, 10, 34, 8, 27, 7}))
+	http.HandleFunc("/primes", handler(primes))
+	http.HandleFunc("/fibo", handler(fibo))
+	http.HandleFunc("/odd", handler(odd))
+	http.HandleFunc("/rand", handler(random))
 
 	log.Fatal(http.ListenAndServe(*listenAddr, nil))
 }
 
-func handler(numbers []int) func(http.ResponseWriter, *http.Request) {
+func handler(fn GeneratorFunc) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		waitPeriod := rand.Intn(550)
-		log.Printf("%s: waiting %dms.", r.URL.Path, waitPeriod)
+		atomic.AddInt64(&limit, 1)
+		defer atomic.AddInt64(&limit, -1)
 
-		time.Sleep(time.Duration(waitPeriod) * time.Millisecond)
+		if atomic.LoadInt64(&limit) > threshold {
+			<-time.After(100 * time.Millisecond)
 
-		x := rand.Intn(100)
-		if x < 10 {
 			http.Error(w, "service unavailable", http.StatusServiceUnavailable)
+			return
+		}
+
+		var (
+			factor int64
+			err    error
+		)
+		// Factor header is only for testing purposes.
+		if factor, err = strconv.ParseInt(r.Header.Get("Factor"), 10, 64); err != nil {
+			switch time.Now().Unix() % 6 {
+			case 0:
+				factor = 1000
+			case 1:
+				factor = 10000
+			case 2:
+				factor = 100000
+			case 3:
+				factor = 1000000
+			case 4:
+				factor = 10000000
+			case 5:
+				factor = 100000000
+			}
+		}
+
+		dl, _ := r.Context().Deadline()
+		log.Printf("%s: request received (factor=%d, deadline=%s, goroutines=%d)", r.URL.String(), factor, dl.String(), atomic.LoadInt64(&limit))
+
+		numbers, err := fn(r.Context(), int(factor))
+
+		if err != nil {
+			switch err {
+			case context.DeadlineExceeded:
+				http.Error(w, "timeout", http.StatusRequestTimeout)
+			case context.Canceled:
+				http.Error(w, "request canceled by the client", http.StatusNoContent)
+			default:
+				http.Error(w, "internal server error", http.StatusInternalServerError)
+			}
 			return
 		}
 
@@ -39,4 +88,72 @@ func handler(numbers []int) func(http.ResponseWriter, *http.Request) {
 
 		json.NewEncoder(w).Encode(map[string]interface{}{"numbers": numbers})
 	}
+}
+
+func primes(ctx context.Context, max int) ([]int, error) {
+	r := make([]int, 0)
+	b := make([]bool, max)
+
+	for i := 2; i < max; i++ {
+		if b[i] == true {
+			continue
+		}
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+			r = append(r, i)
+		}
+		for k := i * i; k < max; k += i {
+			b[k] = true
+		}
+	}
+	return r, nil
+}
+
+func fibo(ctx context.Context, max int) ([]int, error) {
+	var res []int
+
+	for i := 0; i < max; i++ {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+			Phi := (1 + math.Sqrt(5)) / 2
+			phi := (1 - math.Sqrt(5)) / 2
+
+			res = append(res, int((math.Pow(Phi, float64(i))-math.Pow(phi, float64(i)))/math.Sqrt(5)))
+		}
+	}
+	return res, nil
+}
+
+func odd(ctx context.Context, max int) ([]int, error) {
+	var res []int
+	for i := 0; i < max; i++ {
+		if i%2 == 0 {
+			continue
+		}
+
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+			res = append(res, i)
+		}
+	}
+	return res, nil
+}
+
+func random(ctx context.Context, max int) ([]int, error) {
+	var res []int
+	for i := 0; i < max; i++ {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+			res = append(res, rand.Int())
+		}
+	}
+	return res, nil
 }
